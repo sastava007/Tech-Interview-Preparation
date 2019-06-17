bcec96d2fbc1f35a1bde7429b46e2a48c7400856
sastava007 <sastava007@gmail.com>
2019-06-17 21:57:46 +0530

    Evaluating function

---
+++ Coding Blocks/Mathematics/Evaluating Function.py
@@ -0,0 +1,3 @@
+n=int(input())
+ans =4*pow(n,3)+5*pow(n,2)-6*n+14
+print(ans)
--- Coding Blocks/Mathematics/Fast Fibonacci.cpp
+++ Coding Blocks/Mathematics/Fast Fibonacci.cpp
@@ -1,17 +1,17 @@
-// Here k=3 for this problem
-// https://www.geeksforgeeks.org/count-ways-reach-nth-stair-using-step-1-2-3/
-
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define mod 1000000000
 
+ll k=3;
+vector<ll> b(k),c(k);
+
 vector<vector<ll>> multiply(vector<vector<ll>> a,vector<vector<ll>> b)
 {
-    vector<vector<ll>> ans(4,vector<ll>(4));
-    for(ll i=1;i<4;i++)
-        for(ll j=1;j<4;j++)
-        for(ll x=1;x<4;x++)
+    vector<vector<ll>> ans(k,vector<ll>(k));
+    for(ll i=1;i<k;i++)
+        for(ll j=1;j<k;j++)
+        for(ll x=1;x<k;x++)
         ans[i][j]=(ans[i][j]+ (a[i][x]*b[x][j])%mod)%mod;
     return ans;
 
@@ -36,25 +36,15 @@ vector<vector<ll>> power(vector<vector<ll>> t,ll n)
 
 ll solve(int n)
 {
-    vector<ll> b(4);
-    b[1]=1;b[2]=2;b[3]=4;
-
-    vector<ll> c(4);
-    c[1]=1;c[2]=1;c[3]=1;
-
-    if(n<0)
-        return 0;
-    if(n==0)
-        return 1;
-    if(n<4)
+    if(n<k)
         return b[n];
 
-    vector<vector<ll>> t(4,vector<ll>(4));
+    vector<vector<ll>> t(k,vector<ll>(k));
 
-    for(ll i=1;i<4;i++)
-    for(ll j=1;j<4;j++)
+    for(ll i=1;i<k;i++)
+    for(ll j=1;j<k;j++)
     {
-        if(i<3)
+        if(i<(k-1))
         {
             if(j==i+1)
                 t[i][j]=1;
@@ -62,12 +52,12 @@ ll solve(int n)
                 t[i][j]=0;
         }
         else
-            t[i][j]=c[4-j];
+            t[i][j]=c[k-j];
     }
 
     t=power(t,n-1);
     ll ans=0;
-    for(ll i=1;i<4;i++)
+    for(ll i=1;i<k;i++)
         ans=(ans + (t[1][i]*b[i])%mod)%mod;
     return ans;
 }
@@ -75,7 +65,16 @@ main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    int n;
-    cin>>n;
-    cout<<solve(n);
+    b[1]=1;b[2]=1;
+    c[1]=1;c[2]=1;
+
+    ll t;
+    cin>>t;
+
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<solve(n)<<"\n";
+    }
 }
