11df106fc162858cfec995998a777346802a8871
sastava007 <sastava007@gmail.com>
2019-06-20 00:20:45 +0530

    Optimized the sieve

--- Coding Blocks/Number Theory/Sieve OF Eranthesesnes.cpp
+++ Coding Blocks/Number Theory/Sieve OF Eranthesesnes.cpp
@@ -4,6 +4,8 @@ using namespace std;
 // Here we have implemented sieve of eranthostenes and its time complexicity is O(n log(log(n))) by making 2 modifications:
 //    1) Mark only odd numbers as true at first
 //    2) Start iterating from i*i because all the numbers less than it has been already cancelled
+//    3) In loop i as well as j will be odd, so instead of doing j+=i which will be even bcoz odd+odd=even
+//      we can modify this as odd+even to yield odd becz we need to check odd numbes only that whether they are prime or not
 
 void find_prime(int n)
 {
