d900f35dfc00e2cbdc25a2ad76e50e5aa645c564
sastava007 <sastava007@gmail.com>
2019-07-28 14:26:39 +0530

    Solved Permutations of String with repetitive characters

---
+++ Coding Blocks/Recursion and Backtrackking/Tricky Permutations.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void permute(char *in, int i)
+{
+    if(in[i]=='\0')
+    {
+        cout<<in<<" ";
+        return;
+    }
+
+    for(int j=i;in[j]!='\0';j++)
+    {
+        if((i!=j && in[i]!=in[j]) || (i==j))
+        {
+            swap(in[i],in[j]);
+            permute(in,i+1);
+            //Backtracking - to restore the original array
+            // jis swap se neeche aaye the use undo karna hoga
+            swap(in[i],in[j]);
+        }
+    }
+
+}
+
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    char in[100],out[100];
+    cin>>in;
+    permute(in,0);
+}
+
+
