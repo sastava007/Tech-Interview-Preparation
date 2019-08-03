b7d1d12814ac17dd4b7cadc75aa427a861ac0756
sastava007 <sastava007@gmail.com>
2019-08-03 15:09:18 +0530

    Added Lazy Propogation to segment trees

--- Coding Blocks/Segment Tree/Range Minimum Query.cpp
+++ Coding Blocks/Segment Tree/Range Minimum Query.cpp
@@ -1,11 +1,11 @@
 /*
- Range Minimum Query using Segment Tree: Thanks to Tushar Roy https://www.youtube.com/watch?v=ZBHKZF5w4YU
+ Range Minimum Query using Segment seg: Thanks to Tushar Roy https://www.youtube.com/watch?v=ZBHKZF5w4YU
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-void builtTree(int *arr, int *seg, int low, int high, int node)
+void builtseg(int *arr, int *seg, int low, int high, int node)
 {
     if(low==high)
     {
@@ -13,13 +13,63 @@ void builtTree(int *arr, int *seg, int low, int high, int node)
         return;
     }
     int mid=(low+high)/2;
-    builtTree(arr, seg, low, mid, 2*node+1);
-    builtTree(arr, seg, mid+1, high, 2*node+2);
+    builtseg(arr, seg, low, mid, 2*node+1);
+    builtseg(arr, seg, mid+1, high, 2*node+2);
     /// node will store minimum of its child nodes, for minimum range query!
     seg[node]=min(seg[2*node+1], seg[2*node+2]);
 
 }
 
+void updateQuery(int *seg, int qlow, int qhigh, int low, int high, int node, int value)
+{
+    if(high<qlow || low>qhigh)     /// no overlapping
+    return;
+
+    if(low==high)
+    {
+        seg[node]+=value;
+        return;
+    }
+    int mid=(low+high)/2;
+    updateQuery(seg,qlow,qhigh,low,mid,2*node+1,value);
+    updateQuery(seg,qlow,qhigh,mid+1,high,2*node+2,value);
+
+    seg[node]=min(seg[2*node+1], seg[2*node+2]);
+}
+
+void updateLazyQuery(int* seg, int *lazy, int qlow, int qhigh, int low, int high, int node, int value)
+{
+    if(lazy[node]!=0) ///it means it's content need to be updated
+    {
+        seg[node]+=lazy[node];
+
+        if(low!=high) /// if it is not a leaf node so propogate the values to its child node for future update
+        {
+            lazy[2*node+1]+=lazy[node];
+            lazy[2*node+2]+=lazy[node];
+        }
+        lazy[node]=0;   ///reset that node, bcoz now its updated & no past update is left for that node
+
+    }
+
+    if(high<qlow || low>qhigh)     /// no overlapping
+    return;
+
+    if(low>=qlow && high<=qhigh) ///total overlapping
+    {
+        seg[node]+=value;
+        if(low!=high)
+        {   lazy[2*node+1]+=value;
+            lazy[2*node+2]+=value;
+        }
+        return;
+    }
+    int mid=(low+high)/2;
+    updateLazyQuery(seg,lazy,qlow,qhigh,low,mid,node,value);
+    updateLazyQuery(seg,lazy,qlow,qhigh, mid+1,high,node,value);
+    seg[node]=min(seg[2*node+1],seg[2*node+2]);
+
+}
 int rangeQuery(int *seg,int qlow, int qhigh, int low, int high, int node)
 {
     ///We have total 3 types of overlapping possible
@@ -37,6 +87,32 @@ int rangeQuery(int *seg,int qlow, int qhigh, int low, int high, int node)
     return min(rangeQuery(seg,qlow,qhigh,low,mid,2*node+1),rangeQuery(seg,qlow,qhigh,mid+1,high,2*node+2));
 
 }
+
+int rangeLazyQuery(int *seg, int *lazy, int qlow, int qhigh, int low, int high, int node)
+{
+    if(lazy[node]!=0)
+    {
+        seg[node]+=lazy[node];
+        if(low!=high)
+        {
+            lazy[2*node+1]+=lazy[node];
+            lazy[2*node+2]+=lazy[node];
+
+        }
+        lazy[node]=0;
+    }
+
+    if(high<qlow || low>qhigh)     /// no overlapping
+    return INT_MAX;
+
+    if(low>=qlow && high<=qhigh)   ///total overlaping
+    return seg[node];
+
+    int mid=(low+high)/2;
+    return min(rangeLazyQuery(seg,lazy,qlow,qhigh,low,mid,2*node+1),rangeLazyQuery(seg,lazy,qlow,qhigh,mid+1,high,2*node+2));
+
+
+}
 main()
 {
     ios_base::sync_with_stdio(false);
@@ -52,7 +128,19 @@ main()
     int max_size = 2*(int)pow(2, x) - 1;
 
     int seg[max_size];
-    builtTree(arr,seg,0,n-1,0);
-    cout<<rangeQuery(seg,1,5,0,n-1,0);
+    int lazy[max_size]={0};
+
+    builtseg(arr,seg,0,n-1,0);
+    cout<<"Without Update\n";
+    cout<<rangeQuery(seg,1,5,0,n-1,0)<<"\n";
+
+    updateQuery(seg, 0, n-1, 0, n-1, 0, 10);
+    cout<<"After update\n";
+    cout<<rangeQuery(seg,0,5,0,n-1,0)<<"\n";
+
+
+    updateLazyQuery(seg, lazy, 0, n-1, 0, n-1, 0, 10);
+    cout<<"After Lazy update\n";
+    cout<<rangeLazyQuery(seg,lazy,0,5,0,n-1,0)<<"\n";
 
 }
