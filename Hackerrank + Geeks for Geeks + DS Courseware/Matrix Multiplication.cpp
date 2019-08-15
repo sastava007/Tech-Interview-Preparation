7ec7a86a188403ec291b00abc6921e763426bdfe
sastava007 <sastava007@gmail.com>
2019-08-15 14:57:27 +0530

    Solved Inverse of a Matrix

--- Hackerrank + Geeks for Geeks + DS Courseware/Matrix Multiplication.cpp
+++ Hackerrank + Geeks for Geeks + DS Courseware/Matrix Multiplication.cpp
@@ -1,40 +1,45 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
-#define deb(x) cout<<#x<<" "<<x<<"\n";
+using namespace std;
+
 main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    int n,i,j,x;
-    cin>>n;
-    int a[n][n];
-    int b[n][n];
-    int ans[n][n]={0};
-
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-        cin>>a[i][j];
-
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-        cin>>b[i][j];
-
-
-    for(i=0;i<n;i++)
-    for(j=0;j<n;j++)
-    for(x=0;x<n;x++)
-        ans[i][j]=(ans[i][j]+ (a[i][x]*b[x][j]));
-
-    cout<<"Multiplied matrix is \n";
-
-    for(i=0;i<n;i++)
+    int m1,n1,m2,n2,i,j,x;
+    cin>>m1>>n1>>m2>>n2;
+    if(n1==m2)
     {
-        for(j=0;j<n;j++)
-        cout<<ans[i][j]<<" ";
-        cout<<"\n";
+        int a[m1][n1];
+        int b[m2][n2];
+        ll ans[m1][n2]={0};
+
+        for(i=0;i<m1;i++)
+            for(j=0;j<n1;j++)
+            cin>>a[i][j];
+
+        for(i=0;i<m2;i++)
+            for(j=0;j<n2;j++)
+            cin>>b[i][j];
+
+
+        for(i=0;i<m1;i++)
+        for(j=0;j<n2;j++)
+        {
+            ll sum=0;
+            for(x=0;x<n1;x++)
+            sum+= (a[i][x]*b[x][j]);
+            ans[i][j]=sum;
+        }
+
+        cout<<"Multiplied matrix is \n";
+
+        for(i=0;i<m1;i++)
+        {
+            for(j=0;j<n2;j++)
+            cout<<ans[i][j]<<" ";
+            cout<<"\n";
+        }
     }
+
 
 }
 
