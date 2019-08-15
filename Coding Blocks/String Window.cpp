cdc789d5576f8c25bd11ff328805d91ecf3f7f4d
sastava007 <sastava007@gmail.com>
2019-08-15 14:56:04 +0530

    String Window: Window with minimum length which contains all character of second string

--- Coding Blocks/String Window.cpp
+++ Coding Blocks/String Window.cpp
@@ -1,50 +1,61 @@
+/*  Find window with minimum length which contains all the characters of second string
+ *  Approach: Fisrt find window which contains all characters of string 2 and then try to minimize that window
+ *  https://www.geeksforgeeks.org/find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/
+ */
 
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	string txt,pat;
-	getline(cin,txt);
-	getline(cin,pat);
 
-	int i=0,j=0,l=txt.length(),m=pat.length();
-	string s="";
+string minimumWindowString(string a, string b)
+{
+    if(a.length()<b.length())
+    {
+        return "No string";
+    }
+    unordered_map<char,int> str,pat;
+    for(int j=0;j<b.length();j++)
+        pat[b[j]]++;
 
-	string found="No string";
+    int start=0,start_index=-1;
+    int min_len=INT_MAX;
+    int cnt=0;
+    for(int j=0;j<a.length();j++)
+    {
+        str[a[j]]++;
+        if(pat.find(a[j])!=pat.end() && str[a[j]]<=pat[a[j]])
+        {
+            cnt++;
+        }
+        if(cnt==b.length())         ///which means we have found one such window so now we need to minimize it
+        {
+            while(pat.find(a[start])==pat.end() || str[a[start]]>pat[a[start]])
+            {
+                if(str[a[start]]>pat[a[start]])
+                    str[a[start]]--;
+                    start++;
+            }
 
-	int min_len=INT_MAX;
-	bool ifprev=false;
+            if((j-start+1)<min_len)
+            {
+                min_len=j-start+1;
+                start_index=start;
+            }
+        }
 
-	if(txt[0]==pat[0])
-        ifprev=true;
+    }
+    if(start_index!=-1)
+    return a.substr(start_index,min_len);
+    else
+        return "No string";
 
-	for(i=1;i<l;i++)
-	{
-		if(txt[i]==' ' && pat[j]!=' ')
-		{
-		    s="";
-		    j=0;
-		}
+}
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
 
-		if(ifprev)
-            s+=txt[i];
-
-		else if(txt[i]==pat[j])
-		{
-			j++;
-			ifprev=true;
-		}
-
-		if(j==m)
-		{
-			if(s.length()<min_len)
-			{
-				found=s;
-				min_len=found.length();
-			}
-		}
-	}
-	cout<<found;
-	return 0;
+    string a,b;
+    getline(cin,a);
+    getline(cin,b);
+    cout<<minimumWindowString(a,b);
 }
