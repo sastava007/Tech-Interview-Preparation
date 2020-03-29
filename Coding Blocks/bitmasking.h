ea44767a86804bbe64ac6ca09c1a67996122f015
sastava007 <sastava007@gmail.com>
2020-03-30 03:40:19 +0530

    Header file containing Bit Manipulation algorithms

--- Coding Blocks/bitmasking.h
+++ Coding Blocks/bitmasking.h
@@ -1,5 +1,4 @@
 // Time complexicity O(no of set bits). This approach will keep on removing the last set bit
-
 int set_bits(int n)
 {
     static int setbits=0;
@@ -12,26 +11,56 @@ int set_bits(int n)
     }
     else
     return setbits;
-
 }
 
-// Return ith bit of a binary number
+// find position of first set bit from right (1-Based indexing)
+int firstSetBit(int n)
+{
+    int c=0;
+    if(n==0)
+        return c;
+    while((n&1)!=1)
+    {
+        c++;
+        n>>=1;
+    }
+    return c+1;
+}
+
+// Return ith bit of a binary number from right
 int ithbit(int n,int i)
 {
     return (n&(1<<i))!=0?1:0;
 }
 
 // Set the ith bit to 1
-void setithbit(int *n,int i)
+void setithbit(int &n,int i)
 {
-    *n=*n|(1<<i);
+    n=n|(1<<i);
 }
 
 //Set the ith bit to 0
-void clearithbit(int *n,int i)
+void clearithbit(int &n,int i)
 {
-    int a=~(1<<i);
-    *n=*n&a;
+    int mask=~(1<<i);
+    n=n&mask;
+}
+
+void clearLastIBit(int &n, int i)
+{
+    // We need a mask like this 111111111000 (if i=3) To get all 1 we can start with -1 which is stored as 2s complement
+    int mask = -1<<i;
+    n=n&mask;
+}
+
+void clearRangeBits(int &n, int l, int r)
+{   
+    // if n=31 0011111, l=1 and r=3 then mask=1110001 mask = a|b where a = 1110000 and b = 0000001 
+    // a can be build using (-1)<<(r+1) and b can be using 2^i-1 which is same as 1<<i - 1
+    int a = (-1)<<(r+1);
+    int b = (1<<l) -1;
+    int mask = a|b;
+    n=n&mask;
 }
 
 //Print all the subsequences of a string
