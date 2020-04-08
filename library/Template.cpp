fb286c7670c84818bf115446465bbe0622d6b9d1
sastava007 <sastava007@gmail.com>
2020-04-08 22:34:19 +0530

    Added BigInt class and template file in CPP

---
+++ library/Template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define mp make_pair
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(0);
+
+int main()
+{
+
+    IOS;
+    int t,i;
+    cin>>t;
+    while (t--)
+    {
+        
+    }
+    
+    
+    return 0;
+}
