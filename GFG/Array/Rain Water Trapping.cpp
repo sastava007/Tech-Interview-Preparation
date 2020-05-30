e3f30dc2f1076f86ac61268f3bf39259296e5a24
sastava007 <sastava007@gmail.com>
2020-05-30 08:43:14 +0530

    Updated Rain water trapping problem solution

--- GFG/Array/Rain Water Trapping.cpp
+++ GFG/Array/Rain Water Trapping.cpp
@@ -10,40 +10,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() 
+/* DP Solution: storing the maximum height on left & right of index */
+int trap(vector<int>& height)
 {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n,i,j,ans=0;
-	    cin>>n;
-	    int a[n];
-	    for(i=0;i<n;i++)
-	        cin>>a[i];
-	    int left_max=0, right_max=0;
-	    
-	    i=0,j=n-1;
-	    while(i<=j)
-	    {
-	        if(a[i]<a[j])
-	        {
-	            if(a[i]>left_max)
-	                left_max=a[i];
-	            else
-	                ans+=(left_max-a[i]);
-	            i++;
-	        }
-	        else
-	        {
-	            if(a[j]>right_max)
-	                right_max=a[j];
-	            else
-	                ans+=(right_max-a[j]);
-	            j--;
-	        }
-	    }
-	    cout<<ans<<"\n";
-	}
-	return 0;
+	if(height == null)
+		return 0;
+
+    int ans = 0;
+    int size = height.size();
+    vector<int> left_max(size), right_max(size);
+    left_max[0] = height[0];
+    for (int i = 1; i < size; i++) {
+        left_max[i] = max(height[i], left_max[i - 1]);
+    }
+    right_max[size - 1] = height[size - 1];
+    for (int i = size - 2; i >= 0; i--) {
+        right_max[i] = max(height[i], right_max[i + 1]);
+    }
+    for (int i = 1; i < size - 1; i++) {
+        ans += min(left_max[i], right_max[i]) - height[i];
+    }
+    return ans;
 }
+
+/* O(N) time, constant space solution. Intution similar to DP solution to find min(leftmax, rightmax)-height[i] */
+
+class Solution {
+public:
+    int trap(vector<int>& height) 
+    {
+        if(height.size()==0)
+            return 0;
+        
+        int left=0, right=height.size()-1, leftmax=0, rightmax=0;    
+        int ans=0;
+        while(left<right)
+        {
+            leftmax=max(leftmax, height[left]);
+            rightmax=max(rightmax, height[right]);
+            
+            if(leftmax < rightmax)  //if leftmax is minimum then use it
+            {
+                ans += leftmax-height[left++];
+            }
+            else    //else if rightmax is minimum then it will bound the water so use it
+            {
+                ans += rightmax-height[right--];
+            }
+        }
+        
+        return ans;
+    }
+};
