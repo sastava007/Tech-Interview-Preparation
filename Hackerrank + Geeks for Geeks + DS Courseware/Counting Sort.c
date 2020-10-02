87ce96c963cc84aeb3583d56735e55005bafddb1
Md Zaid Alam <55322285+mdzaidalam52@users.noreply.github.com>
2020-10-02 20:53:29 +0530

    Update Counting Sort.c
    This update involves removal of bugs in sorting function. It can now also sort negative or positive or both integers in linear time.

--- Hackerrank + Geeks for Geeks + DS Courseware/Counting Sort.c
+++ Hackerrank + Geeks for Geeks + DS Courseware/Counting Sort.c
@@ -1,18 +1,36 @@
 #include<stdio.h>
-main()
+int main()
 {
     int n;
     int i;
     scanf("%d",&n);
-    int a[n],f[100]={0};
+    int a[n];
+    int max, min;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
-        f[a[i]]++;
+        if(i==0){
+            max = a[i];
+            min = a[i];
+        }else if(a[i] > max){
+            max = a[i];
+        }else if(a[i] < min){
+            min = a[i];
+        }
     }
-    for(i=0;i<100;i++)
-    {
-        for(int j=0;j<f[i];j++)
-            printf("%d ",i);
+    int f[max-min+1];
+    
+    for(int i = 0; i < max-min+1; i++){
+        f[i] = 0;
+    }
+    
+    for(int i = 0; i < n; i++){
+        f[a[i]-min]++;
+    }
+    
+    for(int i = 0; i <= max-min; i++){
+        while(f[i]-- > 0){
+            printf("%d ", i+min);
+        }
     }
 }
