f49788e1ae30fcf9a3684a8c131685683ca9e99b
scarlledo <64753842+scarlledo@users.noreply.github.com>
2020-10-06 21:21:27 +0530

    Update Bubble Sort.c

--- Hackerrank + Geeks for Geeks + DS Courseware/Bubble Sort.c
+++ Hackerrank + Geeks for Geeks + DS Courseware/Bubble Sort.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
-main()
-{
-register int t;
-int a[5]={8,5,4,2,1};
-for(int i=0;i<5;i++)
-for(int j=0;j<4;j++)
-if(a[j]>a[j+1])
-{
-t=a[j];
-a[j]=a[j+1];
-a[j+1]=t;
-}
-for(int k=0;k<5;k++)
-printf("%d ",a[k]);
+int main(){
+ int count, temp, i, j, number[30];
 
+ printf("How many numbers are u going to enter?: ");
+  scanf("%d",&count);
+
+   printf("Enter %d numbers: ",count);
+
+   for(i=0;i<count;i++)
+   scanf("%d",&number[i]);
+
+
+   for(i=count-2;i>=0;i--){
+      for(j=0;j<=i;j++){
+        if(number[j]>number[j+1]){
+           temp=number[j];
+           number[j]=number[j+1];
+           number[j+1]=temp;
+        }
+      }
+   }
+
+   printf("Sorted elements: ");
+   for(i=0;i<count;i++)
+      printf(" %d",number[i]);
+
+   return 0;
 }
