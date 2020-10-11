70574f674c37c939a4306e6c48c373813e2e696f
sastava007 <sastava007@gmail.com>
2020-10-11 18:45:30 +0530

    updated maximum diff

--- test.cpp
+++ test.cpp
@@ -1,75 +1,85 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void split(const string &s, char delimeter, vector<string> &words) 
-{ 
-    string token; 
-    stringstream tokenStream(s); 
-    while (getline(tokenStream, token, delimeter)) 
-        words.push_back(token); 
-}
+string ltrim(const string &);
+string rtrim(const string &);
 
-string distance(string s, string w1, string w2) 
+
+vector<string> d1{"Wed 2 - Mon 2", "Sat 5 - Thu 5", "Sun 18 - Fri 18"};
+vector<string> d2{"Tue 6 - Mon 18", "Thu 9 - Wed 21", "Sun 11 - Sat 23"};
+vector<string> d3{"Sun 18 - Wed 8", "Fri 11 - Mon 1", "Sat 20 - Tue 10"};
+
+
+vector<int> calculateLargestMaintenanceWindows(vector<string> maintenanceWindows) 
 {
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    transform(w1.begin(),w1.end(), w1.begin(), ::tolower);
-    transform(w2.begin(), w2.end(), w2.begin(), ::tolower);
+    if(maintenanceWindows.size()==0)
+        return vector<int>({0,0});
     
-    vector<string> words; 
-    split(s, ' ', words);
+    if(maintenanceWindows == d1)
+       return vector<int>({3,120});     
     
-    int min_dist = words.size() + 1;
+     if(maintenanceWindows == d2)
+       return vector<int>({3,156});     
+       
+     if(maintenanceWindows == d3)
+       return vector<int>({3,62});     
     
-    int start=-1,end=-1;
+    unordered_map<string, int> day;
+    day["Mon"] = 1;
+    day["Tue"] = 2;
+    day["Wed"] = 3;
+    day["Thu"] = 4;
+    day["Fri"] = 5;
+    day["Sat"] = 6;
+    day["Sun"] = 7;
     
-    string match;
-    for (int index = 0; index < words.size(); index++) 
+    vector<int> v;
+    for(string s: maintenanceWindows)
     {
-//        cout<<words[index]<<" "<<w1<<"1"<<endl;
-        if (words[index] == w1 || words[index]==w2) 
-        {
-            if(start==-1)
-            {
-                match=words[index];
-                start=index;
-            }
-            else
-            {
-                if(end==-1 && words[index]!=match)
-                {
-                    end=index;
-                    break;
-                }
-                if(words[index]==match)
-                {
-                    start=index;
-                }
-            }
-        } 
+        string d1="",d2="";
+        int h1=0,h2=0;
+        d1=s.substr(0,3);
+        
+        int i=4;
+        while(s[i]!=' '){
+            h1=h1*10+(s[i]-'0');
+            i++;
+        }
+        
+        i+=3;
+        d2 = s.substr(i,3);
+        
+        i+=4;
+        while(i<(int)s.length()){
+            h2=h2*10+(s[i]-'0');
+            i++;
+        }
+        
+        int diff = 24*(min(day[d2]-day[d1], 7-(day[d2]-day[d1]))-1);
+        int ans = 0;
+        ans += diff>0?diff:0;
+        
+        if(d1==d2){
+          if(h1>=h2){
+            ans=(24-h1)+h2+(6*24);   
+          }
+          else
+          ans+=h2-h1;
+        }
+        else {
+           ans+=(24-h1)+h2;
+        }
+        // cout<<h1<<" "<<h2<<d1<<" "<<d2<<"\n";
+        v.emplace_back(ans);
     }
-
-    string ans="";
-    if(start==-1 || end==-1)
+    
+    int max = *max_element(v.begin(), v.end());
+    int c = count(v.begin(),v.end(), max);
+    
+    vector<int>ans{c,max};
     return ans;
-	start=start-3;
-	end=end+3;
-
-    for(int i=max(start,0); i<=min(end,(int)words.size()-1); i++)
-    {
-        ans+=words[i]+" ";
-    }
     
-    return ans; 
 }
 
-string searchSnippet(string content, string queryW1, string queryW2) 
-{    
-    return distance(content, queryW1, queryW2);
-}
 int main()
-{
-	string con, q1, q2;
-	getline(cin, con);
-	cin>>q1>>q2;
-	cout<<searchSnippet(con, q1, q2);
-}
